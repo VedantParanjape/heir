55367d8915236c85534b72d9ba0f206118351444
vedantparanjape <vedantparanjape160201@gmail.com>
2026-01-14 15:56:45 -0500

    added initial design of attributes to mark recursive functions

--- tools/heir-opt.cpp
+++ tools/heir-opt.cpp
@@ -95,6 +95,7 @@
 #include "lib/Transforms/ShapeInference/ShapeInference.h"
 #include "lib/Transforms/SplitPreprocessing/SplitPreprocessing.h"
 #include "lib/Transforms/StraightLineVectorizer/StraightLineVectorizer.h"
+#include "lib/Transforms/RecursiveCallVectorization/RecursiveCallVectorization.h"
 #include "lib/Transforms/TensorLinalgToAffineLoops/TensorLinalgToAffineLoops.h"
 #include "lib/Transforms/TensorToScalars/TensorToScalars.h"
 #include "lib/Transforms/UnusedMemRef/UnusedMemRef.h"
@@ -289,6 +290,7 @@ int main(int argc, char** argv) {
   registerOperationBalancerPasses();
   registerPopulateScalePasses();
   registerStraightLineVectorizerPasses();
+  registerRecursiveCallVectorizationPasses();
   registerUnusedMemRefPasses();
   registerValidateNoisePasses();
   registerOptimizeRelinearizationPasses();
