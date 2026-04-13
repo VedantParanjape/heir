37bdf8daa73d52104ae102c409439cd7a93b52a5
vedantparanjape <vedantparanjape160201@gmail.com>
2026-04-13 15:25:32 -0400

    changed coyote code to attach layout as attribute to secret.generic block

--- lib/Transforms/CoyoteVectorizer/CoyoteVectorizer.h
+++ lib/Transforms/CoyoteVectorizer/CoyoteVectorizer.h
@@ -7,7 +7,26 @@
 #ifndef LIB_TRANSFORMS_COYOTEVECTORIZER_COYOTEVECTORIZER_H_
 #define LIB_TRANSFORMS_COYOTEVECTORIZER_COYOTEVECTORIZER_H_
 
-#include "mlir/include/mlir/Pass/Pass.h"  // from @llvm-project
+#include "lib/Dialect/Secret/IR/SecretDialect.h"
+#include "lib/Dialect/TensorExt/IR/TensorExtDialect.h"
+#include "lib/Utils/Graph/Graph.h"
+#include "llvm/include/llvm/ADT/DenseMap.h"              // from @llvm-project
+#include "llvm/include/llvm/ADT/DenseSet.h"              // from @llvm-project
+#include "llvm/include/llvm/ADT/EquivalenceClasses.h"    // from @llvm-project
+#include "llvm/include/llvm/ADT/SetVector.h"             // from @llvm-project
+#include "llvm/include/llvm/ADT/SmallSet.h"              // from @llvm-project
+#include "llvm/include/llvm/ADT/SmallVector.h"           // from @llvm-project
+#include "mlir/include/mlir/Dialect/Arith/IR/Arith.h"    // from @llvm-project
+#include "mlir/include/mlir/Dialect/Func/IR/FuncOps.h"   // from @llvm-project
+#include "mlir/include/mlir/Dialect/Tensor/IR/Tensor.h"  // from @llvm-project
+#include "mlir/include/mlir/IR/Builders.h"               // from @llvm-project
+#include "mlir/include/mlir/IR/BuiltinOps.h"             // from @llvm-project
+#include "mlir/include/mlir/IR/PatternMatch.h"           // from @llvm-project
+#include "mlir/include/mlir/IR/Visitors.h"               // from @llvm-project
+#include "mlir/include/mlir/Pass/Pass.h"                 // from @llvm-project
+#include "mlir/include/mlir/Support/LogicalResult.h"     // from @llvm-project
+#include "mlir/include/mlir/Transforms/GreedyPatternRewriteDriver.h"  // from @llvm-project
+#include "mlir/include/mlir/Transforms/RegionUtils.h"  // from @llvm-project
 
 namespace mlir {
 namespace heir {
@@ -17,6 +36,8 @@ namespace heir {
 
 #define GEN_PASS_REGISTRATION
 #include "lib/Transforms/CoyoteVectorizer/CoyoteVectorizer.h.inc"
+
+void coyoteVectorizer(func::FuncOp& func);
 
 }  // namespace heir
 }  // namespace mlir
